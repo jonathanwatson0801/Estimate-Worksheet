0531e1580173bb5efccf4901fe88eec8ac3d887f
jonathanwatson0801 <jonathan.watson0801@gmail.com>
2026-05-11 20:32:09 -0500

    Updated some of the TS Catalog

--- js/tsCatalog.js
+++ js/tsCatalog.js
@@ -3,8 +3,24 @@ const TS_CATALOG = [
     {material: "A21820", dimensions: "2x2x1/8 Angle @ 20'", unit: "Each", unitPrice: 25.44},
     {material: "BHL", dimensions: "Large 7in Barrel Hinges", unit: "PR", unitPrice: 48.02},
     {material: "BHM", dimensions: "Med 5in Barrel Hinges", unit: "PR", unitPrice: 27.40},
-    {material: "A21820", dimensions: "2x2x1/8 Angle @ 20'", unit: "Each", unitPrice: 25.44},
-    {material: "A21820", dimensions: "2x2x1/8 Angle @ 20'", unit: "Each", unitPrice: 25.44},
+    {material: "EALFT37248", dimensions: "ALUM GATE 6X4 FLAT TOP BLACK STRAIGHT", unit: "Each", unitPrice: 200.48},
+    {material: "EALFT37272", dimensions: "ALUM PANEL FLAT TOP BLK 6X6 4-RL 5/8", unit: "Each", unitPrice: 120.17},
+    {material: "EALFT372C", dimensions: "ALUM POST 2X106 CORNER FLAT TOP FOR 6' H", unit: "Each", unitPrice: 30.29},
+    {material: "EALFT372E", dimensions: "ALUM POST 2X106 END FLAT TOP FOR 6' H", unit: "Each", unitPrice: 27.54},
+    {material: "EALFT372G", dimensions: "ALUM POST 2X106 GATE FLAT TOP FOR 6' H", unit: "Each", unitPrice: 37.15},
+    {material: "EALFT372L", dimensions: "ALUM POST 2X106 LINE FLAT TOP FOR 6' H", unit: "Each", unitPrice: 40.86},
+    {material: "EX349F4896", dimensions: "EXP METAL FLAT 3/4in # 9 STK 4 X 8", unit: "Each", unitPrice: 66.37},
+    {material: "FW12008", dimensions: "CLASSIC BLACK 48 X 96 PANEL", unit: "Each", unitPrice: 55.23},
+    {material: "FW12010", dimensions: "CLASSIC BLACK 72 X 96 PANEL", unit: "Each", unitPrice: 76.76},
+    {material: "FW12410", dimensions: "3in BLACK BRACKET", unit: "Each", unitPrice: 2.63},
+    {material: "FW12896", dimensions: "3in X 8 FT BLACK POST", unit: "Each", unitPrice: 68.87},
+    {material: "FW22717", dimensions: "3in X 3in POST CAP, PLASTIC, DESIGNMASTER", unit: "Each", unitPrice: 1.47},
+    {material: "FW63946", dimensions: "3in X 6 FT BLACK POST", unit: "Each", unitPrice: 48.44},
+    {material: "FW64874", dimensions: "CONTEMPO BLACK 48 X 98.4 PANEL", unit: "Each", unitPrice: 143.33},
+    {material: "GANV3", dimensions: "FLAT MOUNT RESIDENTIAL BRACKET", unit: "Each", unitPrice: 1.75},
+    {material: "GANVC3", dimensions: "FLAT MOUNT COMMERCIAL BRACKET", unit: "Each", unitPrice: 2.00},
+    {material: "", dimensions: "", unit: "Each", unitPrice: 0.00},
+
 
 
 
